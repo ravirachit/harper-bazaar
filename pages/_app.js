dddbaad8ae8387902294fbbed7349651d25fd298
unknown <admin@NODFC20LT210.tvtoday.net>
2021-04-22 22:26:46 +0530

    for deployment

--- pages/_app.js
+++ pages/_app.js
@@ -13,25 +13,3 @@ function MyApp({ Component, pageProps }) {
 
 export default MyApp
 
-
-
-// Basic commands for next
-
-// To create build
-// npm run build
-
-// To start npm
-// npm run start
-
-// To run on dev
-// npm run dev  
-
-
-// "scripts": {
-//   "dev": "next dev",
-//   "build": "next build",
-//   "start": "next start -H 0.0.0.0 -p $PORT",
-//   "start:dev": "next start",
-//   "start:prod": "next start -p 80",
-//   "export": "next build && next export"
-// },
